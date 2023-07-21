97a4c8afcd2bb02cbace31ea6e6050879d200fa4
sunit pal <sunitpal2000@gmail.com>
2023-07-21 23:32:57 +0530

    add props

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
-import Note from "./note";
+import Note from "./note.jsx";
+import Notes from "../notes";
 
 function app() {
   return (
     <div>
       <Header />
-      <Note />
+      {Notes.map((eachNote) => (
+        <Note
+          key={eachNote.key}
+          title={eachNote.title}
+          content={eachNote.content}
+        />
+      ))}
       <Footer />
     </div>
   );
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "../components_styles/note.css";
 
-function note() {
+function note(props) {
   return (
     <div className="note">
-      <h1>Note Title</h1>
-      <p>Note Content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
